1ac8637035fcaddcfda5220c78271952c3f7781f
Louis MARTIN <louisrtm@gmail.com>
2018-05-08 20:10:08 +0000

    Increase difficulty from easy to medium

--- habitSync.js
+++ habitSync.js
@@ -159,6 +159,7 @@ habitSync.prototype.syncItemsToHabitRpg = function(items, cb) {
           text: item.todoist.content,
           dateCreated: new Date(item.todoist.date_added),
           date: dueDate,
+          priority: "1.5",
           type: taskType.type,
           repeat: taskType.repeat,
           completed: item.todoist.checked == true
